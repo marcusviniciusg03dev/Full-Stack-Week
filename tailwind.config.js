a68d3021caa472ed08383a84be939174e0a6858f
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-05 01:46:25 -0300

    chore: set theme colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: '#590BD8',
+        primaryDarker: '#312A4F',
+        primaryLighter: '#DDD5EA',
+        greyPrimary: '#717171',
+        greyLighter: '#BBBFBF',
+      },
+      textColor: {
+        dark: '#717171'
+      }
     },
   },
   plugins: [],
