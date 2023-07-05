e73fd412ff13c79168c81fcd998d5c5f42277cb1
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-05 16:45:18 -0300

    feat: add trip search to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import TripSearch from "./components/TripSearch";
+
 export default function Home() {
   return (
-    <h1>Home Page</h1>
+    <div>
+      <TripSearch />
+    </div>
   )
 }
