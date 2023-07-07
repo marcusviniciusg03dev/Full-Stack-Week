8fa5ef855aa284ef2b135544f7aef0fb675264ae
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-06 21:38:56 -0300

    style: add margin bottom to trip highlights

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -7,7 +7,7 @@ interface TripHighlightsProps {
 const TripHighlights = ({ highlights }: TripHighlightsProps) => {
     return (
         <div className="flex flex-col p-5">
-            <h2 className="font-semibold text-primaryDarker">Destaques</h2>
+            <h2 className="font-semibold text-primaryDarker mb-2">Destaques</h2>
             
             <div className="flex flex-wrap gap-y-3">
                 {highlights.map(highlight => (
