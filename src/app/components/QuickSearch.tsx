2d7c71b0d7c38933f19c2b9dbbf4e46d9a2d38ad
Marcus Vinicius Gomes <marcusvinid3v@gmail.com>
2023-07-12 00:40:11 -0300

    style: adapt home page to desktop

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -10,12 +10,12 @@ const QuickSearch = () => {
                 <div className="w-full h-[1px] bg-grayLighter"></div>
             </div>
 
-            <div className="flex w-full justify-between mt-5">
+            <div className="flex w-full justify-between mt-5 lg:mt-10 lg:justify-center gap-40">
                 <div className="flex flex-col items-center gap-1">
                     <Link href="/trips/search?text=hotel" className="flex flex-col items-center hover:text-primary transition-all">
                         <Image width={35} height={35} src="/hotel-icon.png" alt="Hotel" />
 
-                        <p className="text-sm text-grayPrimary">Hotel</p>
+                        <p className="text-sm text-grayPrimary lg:text-base">Hotel</p>
                     </Link>
                 </div>
 
@@ -23,7 +23,7 @@ const QuickSearch = () => {
                     <Link href="/trips/search?text=fazenda" className="flex flex-col items-center hover:text-primary transition-all">
                         <Image width={35} height={35} src="/farm-icon.png" alt="Fazenda" />
 
-                        <p className="text-sm text-grayPrimary">Fazenda</p>
+                        <p className="text-sm text-grayPrimary lg:text-base">Fazenda</p>
                     </Link>
                 </div>
 
@@ -31,7 +31,7 @@ const QuickSearch = () => {
                     <Link href="/trips/search?text=chalé" className="flex flex-col items-center hover:text-primary transition-all">
                         <Image width={35} height={35} src="/cottage-icon.png" alt="Chalé" />
 
-                        <p className="text-sm text-grayPrimary">Chalé</p>
+                        <p className="text-sm text-grayPrimary lg:text-base">Chalé</p>
                     </Link>
                 </div>
 
@@ -39,7 +39,7 @@ const QuickSearch = () => {
                     <Link href="/trips/search?text=pousada" className="flex flex-col items-center hover:text-primary transition-all">
                         <Image width={35} height={35} src="/inn-icon.png" alt="Pousada" />
 
-                        <p className="text-sm text-grayPrimary">Pousada</p>
+                        <p className="text-sm text-grayPrimary lg:text-base">Pousada</p>
                     </Link>
                 </div>
             </div>
--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -27,12 +27,12 @@ const TripSearch = () => {
     }
 
     return (
-        <div className="container mx-auto p-5">
-            <h1 className="font-semibold text-2xl text-primaryDarker text-center">
+        <div className="container mx-auto p-5 lg:py-28 bg-[#00000008] bg-search-background bg-cover bg-no-repeat bg-center">
+            <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
                 Encontre sua próxima <span className="text-primary">viagem!</span>
             </h1>
 
-            <div className="flex flex-col gap-4 mt-5">
+            <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
                 <Input
                     placeholder="Onde você quer ir?"
                     error={!!errors.text}
@@ -47,7 +47,7 @@ const TripSearch = () => {
                     }
                 />
 
-                <div className="flex gap-4">
+                <div className="flex gap-4 lg:w-full">
                     <Controller
                         name="startDate"
                         control={control}
@@ -76,7 +76,7 @@ const TripSearch = () => {
                     />
                 </div>
 
-                <Button onClick={() => handleSubmit(onSubmit)()}>Buscar</Button>
+                <Button onClick={() => handleSubmit(onSubmit)()} className="w-1/2">Buscar</Button>
             </div>
         </div>
     );
